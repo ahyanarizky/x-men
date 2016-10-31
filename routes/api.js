93b86746837723f2c5dac171dd18a242009e3871
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-10-31 21:23:33 +0700

    refactor mvp

--- routes/api.js
+++ routes/api.js
@@ -4,113 +4,21 @@ var models = require('../models')
 var mutan = models.Mutant
 var skill = models.Skill
 var mutanSkill = models.MutantSkill
+var controller = require('../controller/controller')
 /* GET users listing. */
 //read all mutant
-router.get('/mutants', function(req, res, next) {
-    mutan.findAll({order: ['id']}).then((data) => {
-        res.json(data)
-    })
-});
+router.get('/mutants', controller.getAllMutants);
+router.post('/mutants', controller.createNewMutant);
+router.put('/mutants/:id', controller.reconstructMutant)
+router.delete('/mutants/:id', controller.eliminateMutant)
 
-router.post('/mutants', function(req, res, next) {
-    mutan.create({name: req.body.name}).then((data) => {
-        res.json(data)
-    })
-});
+router.get('/skills', controller.getAllSkills);
+router.post('/skills', controller.developNewSkill);
+router.put('/skills/:id', controller.reconstructSkill)
+router.delete('/skills/:id', controller.eliminateSkill)
 
-router.put('/mutants/:id', function(req, res, next) {
-    mutan.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then((data) => {
-        data.update({name: req.body.name}).then((result) => {
-            res.json(result)
-        })
-    })
-})
-
-router.delete('/mutants/:id', function(req, res, next) {
-    mutan.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then((data) => {
-        if (data > 0) {
-            res.json({message: 'Delete successfully'})
-        } else {
-            res.json({message: 'ID not found'})
-        }
-        res.json(data)
-    })
-})
-
-router.get('/skills', function(req, res, next) {
-    skill.findAll({order: ['id']}).then((data) => {
-        res.json(data)
-    })
-});
-
-router.post('/skills', function(req, res, next) {
-    skill.create({power: req.body.power, level: req.body.level}).then((data) => {
-        res.json(data)
-    })
-});
-
-router.put('/skills/:id', function(req, res, next) {
-    skill.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then((data) => {
-        data.update({power: req.body.power, level: req.body.level}).then((result) => {
-            res.json(result)
-        })
-    })
-})
-
-router.delete('/skills/:id', function(req, res, next) {
-    skill.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then((data) => {
-        if (data > 0) {
-            res.json({message: 'Delete successfully'})
-        } else {
-            res.json({message: 'ID not found'})
-        }
-        res.json(data)
-    })
-})
-
-router.get('/mutantskill', function(req, res, next) {
-    mutan.findAll({
-        include: [
-            {
-                model: skill,
-                through: {
-                    // atttributes: ['power'],
-                    where: {
-                        MutantId: 12
-                    }
-                }
-            }
-        ]
-    }).then((data) => {
-        for (var i = 0; i < data.length; i++) {
-            if (data[i].dataValues.id == 12) {
-                res.json(data[i].dataValues)
-            } else {}
-        }
-    })
-})
-
-router.post('/mutantskill', function(req, res, next) {
-    mutanSkill.create({MutantId: req.body.MutantId, SkillId: req.body.SkillId, level: req.body.level}).then((data) => {
-        res.json(data)
-    })
-})
+router.get('/mutantskill/:id', controller.showMutantSkills)
+router.post('/mutantskill', controller.trainMutant)
 
 router.get('/mutantpower/:id', function(req, res, next) {})
 
